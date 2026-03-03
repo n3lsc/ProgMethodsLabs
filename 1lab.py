ea5ad957f3e5ab22b85c3e3a7beaa35bb5df8bf4
n3lsc <nellalyse2@yandex.ru>
2026-03-03 18:29:50 +0700

    То, как проходила защита первой лабораторной работы

--- 1lab.py
+++ 1lab.py
@@ -25,13 +25,16 @@ def fst_space_in_string(inp_str):
 def cort_string(inp_str):
     pos = fst_space_in_string(inp_str)
     return (inp_str[0: pos], inp_str[pos: -1])
-    
+
+'''
 def menu():
     items = []
     while True:
-        print(" Меню для управления курсами валют ")
-        print(" 1. Добавить новый объект          ")
-        print(" 2. Удалить существующий объект    " )
+        print("===================================" \
+          " Меню для управления курсами валют " \
+          " 1. Добавить новый объект          " \
+          " 2. Удалить существующий объект    " \
+          "===================================")
         
         menuItem = int(input())
         if menuItem == 1:
@@ -44,7 +47,12 @@ def menu():
             # ...
             print(items)
 
+'''
 def main():
+    inp_str = "Hello, World!"
+    print(cort_string(inp_str))
+
+    '''
     if len(sys.argv) != 6:
         print("python 1lab.py цвет валюта1 валюта2 курс гггг.мм.дд")
         return
@@ -57,7 +65,8 @@ def main():
     obj = CurrencyRates(color, currency1, currency2, rate, date)
     print("Создан объект:")
     print(obj)
-
+    '''
 if __name__ == "__main__":
     main()
-    menu()
+
+'''menu()'''
