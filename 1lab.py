1e390ff74e2faaacec2fb38969423ff637807b1f
n3lsc <nellalyse2@yandex.ru>
2026-03-03 17:37:32 +0700

    added field color for obj

--- 1lab.py
+++ 1lab.py
@@ -4,24 +4,26 @@ import sys
 
 @dataclass
 class CurrencyRates:
+    color: str
     currency_name_1: str
     currency_name_2: str
     rate: float
     date: datetime.date
 
     def __str__(self):
-        return (f"Курс валюты {self.currency_name_1} к {self.currency_name_2} на дату {self.date} составляет {self.rate}")
+        return (f"Цвет связки {self.color} и курс валюты {self.currency_name_1} к {self.currency_name_2} на дату {self.date} составляет {self.rate}")
     
 def main():
-    if len(sys.argv) != 5:
-        print("python 1lab.py валюта1 валюта2 курс гггг.мм.дд")
+    if len(sys.argv) != 6:
+        print("python 1lab.py цвет валюта1 валюта2 курс гггг.мм.дд")
         return
-    currency1 = sys.argv[1]
-    currency2 = sys.argv[2]
-    rate = float(sys.argv[3])
-    date = datetime.strptime(sys.argv[4], "%Y.%m.%d").date()
+    color = sys.argv[1]
+    currency1 = sys.argv[2]
+    currency2 = sys.argv[3]
+    rate = float(sys.argv[4])
+    date = datetime.strptime(sys.argv[5], "%Y.%m.%d").date()
 
-    obj = CurrencyRates(currency1, currency2, rate, date)
+    obj = CurrencyRates(color, currency1, currency2, rate, date)
     print("Создан объект:")
     print(obj)
 
