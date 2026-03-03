5710be88c30e625a750370b8968db0376835c884
n3lsc <nellalyse2@yandex.ru>
2026-03-03 18:27:17 +0700

    improved menu func (still doesn't work)

--- 1lab.py
+++ 1lab.py
@@ -10,15 +10,39 @@ class CurrencyRates:
     rate: float
     date: datetime.date
 
+
+
     def __str__(self):
         return (f"Цвет связки {self.color} и курс валюты {self.currency_name_1} к {self.currency_name_2} на дату {self.date} составляет {self.rate}")
+
+def fst_space_in_string(inp_str):
+    counter = 0
+    for i in inp_str:
+        counter += 1
+        if i == " ":
+            return counter
+
+def cort_string(inp_str):
+    pos = fst_space_in_string(inp_str)
+    return (inp_str[0: pos], inp_str[pos: -1])
     
 def menu():
-    print("===================================" \
-          " Меню для управления курсами валют " \
-          " 1. Добавить новый объект          " \
-          " 2. Удалить существующий объект    " \
-          "===================================")
+    items = []
+    while True:
+        print(" Меню для управления курсами валют ")
+        print(" 1. Добавить новый объект          ")
+        print(" 2. Удалить существующий объект    " )
+        
+        menuItem = int(input())
+        if menuItem == 1:
+            # ...
+            string = input("enter data: ")
+
+            True
+            # items.append(...)
+        elif menuItem == 2:
+            # ...
+            print(items)
 
 def main():
     if len(sys.argv) != 6:
@@ -36,3 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    menu()
